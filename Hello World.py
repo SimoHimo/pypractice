6f5f5cfda516ae182449f9f66717b0148855e87e
Syed Mishkatul Haque <xsimohimox@gmail.com>
2024-02-09 20:43:15 +0600

    A hello world file, or is it?

--- Hello World.py
+++ Hello World.py
@@ -3,7 +3,7 @@ import time
 # alphabets = list(string.ascii_letters)
 # alphabets.exteend(list(string.punctuation))
 alphabets = [' ','!','a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z']
-myMessege = "Hello_World!"
+myMessege = "Hello World!"
 newMessege = ' '
 index = 0
 
